575b4b5773d1d20994ab94f789d4b8402d6098af
Trần Vũ Trường <truongtv1399it@gmail.com>
2022-10-23 10:20:15 +0700

    Update project

---
+++ src/programming/Functional02.java
@@ -0,0 +1,54 @@
+package programming;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class Functional02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub\
+		List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+		
+		Predicate<Integer> predicateNUmber = x -> x%2==0;
+		
+		Predicate<Integer> predicateNUmber2 = new Predicate<Integer>() {
+			@Override
+			public boolean test(Integer x) {
+				return x%2==0;
+			}
+		};
+		
+		
+		Function<Integer,Integer> mapperNumber = x -> x*x;
+		
+		Function<Integer,Integer> mapperNumber2 = new Function<Integer, Integer>() {
+			
+			@Override
+			public Integer apply(Integer x) {
+				// TODO Auto-generated method stub
+				return x*x;
+			}
+		};
+		
+		
+		Consumer<Integer> actionNumber = System.out::println;
+		
+		
+		Consumer<Integer> actionNumber2 = new Consumer<Integer>() {
+			@Override
+			public void accept(Integer t) {
+				System.out.println(t);
+				
+			}
+		};
+		
+		numbers.stream()
+		.filter(predicateNUmber2)
+		.map(mapperNumber2)
+		.forEach(actionNumber2);
+		
+	}
+
+}
